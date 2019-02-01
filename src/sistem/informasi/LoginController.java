b40ec218dcf6abf4fe22b7f1189a75ef995febf0
Duhan f <ASUS@ASUS-PC>
2019-02-01 10:38:31 +0700

    Login view KKP

---
+++ src/sistem/informasi/LoginController.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistem.informasi;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author ASUS
+ */
+public class LoginController implements Initializable {
+    
+   @FXML
+    private JFXTextField username;
+
+    @FXML
+    private JFXPasswordField pass;
+
+    @FXML
+    private JFXButton login;
+
+    @FXML
+    private JFXButton cancel;
+
+    @FXML
+    void tombolCancel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void tombolLogin(ActionEvent event) {
+
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
